93255bea8ad1f1152b1ac5d7c67cd9555023ee60
zachfrosty4986 <zachjames0114@gmail.com>
2024-06-13 16:43:50 -0400

    Changed userRoutes.js to loinRoutes.js Changed loginRoutes.js to landingRoutes.js changed User.js to Login.js Added content for login configuration into loginRoutes.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 // Import the routes. This is how we make our routes modular.
-const userRoutes = require('./userRoutes');
+const userRoutes = require('./loginRoutes');
 const blogRoutes = require('./blogRoutes');
 
 // When a request is made to the /users or /projects path, it will be directed to the index.js in the /users or /projects folder.
