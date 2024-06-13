f9847e9cc12635cf48f55969f1134fa0925ff57c
zachfrosty4986 <zachjames0114@gmail.com>
2024-06-13 16:37:01 -0400

    Debugged routes, added loginRoutes file

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
+const homeRoutes = require('./loginRoutes');
 
-router.use('/', homeRoutes);
+router.use('/', loginRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
