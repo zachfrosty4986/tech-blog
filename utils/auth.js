251cbd49f352a8586fb1cf869c0583b5493ef467
zachfrosty4986 <zachjames0114@gmail.com>
2024-06-30 16:15:34 -0400

    Added utils folder with helpers.js and auth.js for login functionality and date posting

---
+++ utils/auth.js
@@ -0,0 +1,11 @@
+const isAuthorized = (req, res, next) => {
+    // redirect the user to log in before doing any api request where this is added
+    if (!req.session.logged_in) {
+      res.redirect('/login');
+    } else {
+      // next moves the request to the next step
+      next();
+    }
+  };
+  
+  module.exports = isAuthorized;
